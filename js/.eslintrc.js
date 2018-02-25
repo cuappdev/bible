d628e2cce85096c1834c948b68e4d7eb4c9377ba
Megan Le <meganle7@gmail.com>
2018-02-25 00:26:47 -0500

    eslint support for jest

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   'env': {
     'browser': true,
     'es6': true,
-    'jquery': true
+    'jquery': true,
+    'jest': true
   },
   'extends': 'semistandard',
   'parserOptions': {
